705c2f7e7134635910f5453c5408969a8a17d262
Voja123 <markovicvojislav101@gmail.com>
2026-02-03 23:40:18 +0100

    error

--- backend/utils/errors.go
+++ backend/utils/errors.go
@@ -1,1 +1,27 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// ErrorResponse represents a JSON error response
+type ErrorResponse struct {
+	Error   string `json:"error"`
+	Message string `json:"message,omitempty"`
+}
+
+// JSONError sends a JSON error response
+func JSONError(w http.ResponseWriter, message string, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	
+	response := ErrorResponse{
+		Error:   http.StatusText(statusCode),
+		Message: message,
+	}
+	
+	json.NewEncoder(w).Encode(response)
+}
+
 
